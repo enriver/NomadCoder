93263d6b49856da63c6b0fec2f9cae736acfaa6e
enriver <riverkeh@naver.com>
2021-03-03 16:31:19 +0900

    0303

--- Movie_App/src/App.js
+++ Movie_App/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 class App extends React.Component{
-  state={
-    count:0
-  };
-  render (){
-    return(
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button>Add</button>
-      <button>Minus</button>
-    </div>
-    );
-  }
+ state={
+   isLoading:true,
+   movies:[]
+ }
+ componentDidMount(){
+   setTimeout(()=>{
+     this.setState({isLoading : false});
+   }, 6000);
+ }
+ render(){
+   const {isLoading}=this.state;
+   return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+ }
 }
 
 export default App;
